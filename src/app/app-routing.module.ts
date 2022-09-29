3255c1da7a607aafc6f34300dcfb0af3be361609
Douglas <toddynotes@gmail.com>
2022-09-28 23:55:38 -0300

    refactor(package): update do angular e inserido rota para projects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
+import { RouterModule, Routes } from '@angular/router';
+
 import { ProjectsPageComponent } from './projects-page/projects-page.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: '/projects', component: ProjectsPageComponent}
+  {path: '', component: HomeComponent, title: 'Douglas Portfolio - About me'},
+  {path: 'projects', component: ProjectsPageComponent, title: 'Douglas Portfolio - My Projects'}
 ];
 
 @NgModule({
